ca9955c5209cca96aaee384a49f17c2543a7728c
Igor Sodre <igorcuei@yahoo.com.br>
2020-02-23 02:36:16 -0300

    ISSUE #25 - ADD Generic Complexity Calculator and method that runs it in all tasks

--- src/complexity_functions/css_complexity.ts
+++ src/complexity_functions/css_complexity.ts
@@ -5,17 +5,19 @@ import fs from 'fs';
  * @param FILE_PATH A path to a file
  */
 export default function calculateCSSComplexity(FILE_PATH: string): TFileComplexity {
+    // if (!fs.existsSync(FILE_PATH)) return null;
+
+    // Esta linha eh somente para propositos de teste
+    if (!fs.existsSync(FILE_PATH)) return 'BAIXA';
     let css_file = fs.readFileSync(FILE_PATH, 'utf8');
 
     let quantity_of_objects = css_file.split('}').length - 1;
 
     if (quantity_of_objects < 30) {
         return 'BAIXA';
-    }
-    else if (quantity_of_objects > 30 && quantity_of_objects < 60) {
+    } else if (quantity_of_objects > 30 && quantity_of_objects < 60) {
         return 'MEDIA';
-    }
-    else {
+    } else {
         return 'ALTA';
     }
 }
