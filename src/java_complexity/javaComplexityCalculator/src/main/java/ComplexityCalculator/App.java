f61445700a787628a5722ca182e81eff8d987128
Igor Sodre <csodreigor@gmail.com>
2020-05-04 23:19:46 -0300

    ISSUE #2 - Add count for number of variables, number of API expositions and number of IIB calls

--- src/java_complexity/javaComplexityCalculator/src/main/java/ComplexityCalculator/App.java
+++ src/java_complexity/javaComplexityCalculator/src/main/java/ComplexityCalculator/App.java
@@ -8,7 +8,14 @@ import java.nio.file.Paths;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.symbolsolver.resolution.typeinference.Instantiation;
+
+import org.checkerframework.common.reflection.qual.NewInstance;
+
+import javassist.expr.MethodCall;
 
 /**
  * App!
@@ -18,18 +25,46 @@ public class App {
     public String testfieldDecl;
 
     public static void main(String[] args) throws IOException {
-        String testFileString = Files.readString(Paths.get("./javaComplexityCalculator/src/examples/example.txt"),
-                StandardCharsets.UTF_8);
+        String filePath = "./javaComplexityCalculator/src/examples/example5.txt";
+        calculateJavaComplexity(filePath);
+    }
 
-        CompilationUnit compilationUnit = StaticJavaParser.parse(testFileString);
-        System.out.println("Teste1");
-        compilationUnit.findAll(VariableDeclarationExpr.class).stream().forEach(f -> {
-            System.out.println(f.toString());
-        });
-        System.out.println("=============================================");
-        System.out.println("Teste2");
-        compilationUnit.findAll(FieldDeclaration.class).stream().forEach(f -> {
-            System.out.println(f.toString());
-        });
+    private static String calculateJavaComplexity(String filePath) throws IOException {
+        String fileCode = null;
+        try {
+            fileCode = Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (Exception err) {
+            System.out.println(err.getMessage());
+            return null;
+        }
+
+        // Parse code
+        CompilationUnit compilationUnit = StaticJavaParser.parse(fileCode);
+        long numOfVariables = 0;
+        long numOfApiExpositions = 0;
+        long numOfIIBCalls = 0;
+
+        // Count number of variables
+        numOfVariables += compilationUnit.findAll(VariableDeclarationExpr.class).stream().count();
+        numOfVariables += compilationUnit.findAll(FieldDeclaration.class).stream().count();
+
+        // Count API expositions
+        for (MethodDeclaration m : compilationUnit.findAll(MethodDeclaration.class)) {
+            String dlc = m.toString();
+            if (dlc.toUpperCase().contains("@PATH"))
+                numOfApiExpositions++;
+        }
+
+        // Count Object IIB calls
+        for (MethodCallExpr m : compilationUnit.findAll(MethodCallExpr.class)) {
+            String dlc = m.toString();
+            if (dlc.toUpperCase().contains("GETRESPOSTA()"))
+                numOfIIBCalls++;
+        }
+
+        System.out.println("Num of variables: " + numOfVariables);
+        System.out.println("Num of API expositions: " + numOfApiExpositions);
+        System.out.println("Num of IIB calls: " + numOfIIBCalls);
+        return null;
     }
 }
