984d84b1e46d5b169325ab077520ceca75835e5b
Igor Sodre <csodreigor@gmail.com>
2020-05-06 20:09:12 -0300

    ISSUE #2 - Improve way of counting IIB calls, fix typos on readme, try to add support for single project report generation, update .jar

--- src/java_complexity/javaComplexityCalculator/src/main/java/ComplexityCalculator/App.java
+++ src/java_complexity/javaComplexityCalculator/src/main/java/ComplexityCalculator/App.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -17,7 +19,7 @@ import com.github.javaparser.ast.expr.VariableDeclarationExpr;
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        String filePath = "./src/examples/example5.txt";
+        String filePath = "src/examples/example8.txt";
         calculateJavaComplexity(filePath);
     }
 
@@ -38,29 +40,50 @@ public class App {
 
         // Count number of variables
         numOfVariables += compilationUnit.findAll(VariableDeclarationExpr.class).stream().count();
-        numOfVariables += compilationUnit.findAll(FieldDeclaration.class).stream().count();
+        List<String> utilizedOperations = new ArrayList<>();
+        for (FieldDeclaration field : compilationUnit.findAll(FieldDeclaration.class)) {
+            numOfVariables++;
+            String dlc = field.toString().toUpperCase();
+            if (dlc.contains("@INJECT") && dlc.contains("OPERACAO")) {
+                String[] l = dlc.split(" ");
+                String operation = l[l.length - 1].replace(";", "");
+                utilizedOperations.add(operation);
+            }
+
+        }
 
         // Count API expositions
         for (MethodDeclaration m : compilationUnit.findAll(MethodDeclaration.class)) {
-            String dlc = m.toString();
-            if (dlc.toUpperCase().contains("@PATH"))
+            String dlc = m.toString().toUpperCase();
+            if (dlc.contains("@PATH"))
                 numOfApiExpositions++;
         }
 
         // Count Object IIB calls and Files
         for (MethodCallExpr m : compilationUnit.findAll(MethodCallExpr.class)) {
-            String dlc = m.toString();
-            if (dlc.toUpperCase().contains("GETRESPOSTA()"))
+            String dlc = m.toString().toUpperCase();
+            if (dlc.contains("GETRESPOSTA()"))
                 numOfIIBCalls++;
-            if (dlc.toUpperCase().contains("FILES")
-                || dlc.toUpperCase().contains("NEW FILE")
-                || dlc.toUpperCase().contains("NEW FILEINPUTSTREAM")
-                || dlc.toUpperCase().contains("NEW FILEREADER")
-                || dlc.toUpperCase().contains("NEW RANDOMACCESSFILE"))
+
+            for (String s : utilizedOperations) {
+                if (dlc.contains((s + ".").replace(" ", ""))) {
+                    numOfIIBCalls++;
+                    break;
+                }
+            }
+            if (dlc.contains("FILES")
+                    || dlc.contains("NEW FILE")
+                    || dlc.contains("NEW FILEINPUTSTREAM")
+                    || dlc.contains("NEW FILEREADER")
+                    || dlc.contains("NEW RANDOMACCESSFILE"))
                 numOfOpenedFiles++;
         }
 
         long totalComplexity = (numOfVariables / 5) + numOfApiExpositions + numOfIIBCalls + numOfOpenedFiles;
+//        System.out.println("numOfVariables: " + numOfVariables);
+//        System.out.println("numOfApiExpositions: " + numOfApiExpositions);
+//        System.out.println("numOfIIBCalls: " + numOfIIBCalls);
+//        System.out.println("numOfOpenedFiles: " + numOfOpenedFiles);
         System.out.println("File and Total complexity: " + filePath + " -> " + totalComplexity);
 
         if (totalComplexity <= 15)
