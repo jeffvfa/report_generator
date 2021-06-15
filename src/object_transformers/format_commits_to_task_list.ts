7faae58b003edb76baa0d9b902ce68684f1ec646
C1300499 Jefferson Viana Fonseca Abreu <c1300499@interno.bb.com.br>
2021-06-15 14:59:26 -0300

    restrict the hash size to 10 digits

--- src/object_transformers/format_commits_to_task_list.ts
+++ src/object_transformers/format_commits_to_task_list.ts
@@ -14,7 +14,7 @@ const buildFileObjects = (y: IGitLogOutput, projectPath: string, retrieveCategor
         // build file objects
         .map(el => {
             let elSplited = el.split(' ');
-            const filePath = projectPath + elSplited[1] + '#' + y.commit;
+            const filePath = projectPath + elSplited[1] + '#' + y.commit.slice(0,10);
 
             return {
                 diffType: elSplited[0],
